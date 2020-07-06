5b4bb409660bb983c7d84dc940244895eacbca79
YOUR_VAMSI <vamsir@student.chalmers.se>
2020-07-06 21:39:37 +0200

    Tournament model

---
+++ backend/model/tournament.js
@@ -0,0 +1,26 @@
+
+
+class tournament{
+
+constructor(json){
+
+}
+
+id = 0
+tournamentName = String
+numberOfTeams = 0
+sportsType = String
+startDate= String
+endDate = String
+
+}
+
+const sportsType = {
+    1: 'Soccer',
+    2: 'Handball',
+    3: 'Cricket'
+}
+
+module.exports = {
+    addNewSportsType
+}
