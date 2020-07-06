39ef4fdb20ee66e5136cf3751156b1cfa08efbbc
YOUR_VAMSI <vamsir@student.chalmers.se>
2020-07-06 22:07:22 +0200

    Group model

---
+++ backend/model/group.js
@@ -0,0 +1,21 @@
+
+class group{
+
+constructor(json){
+
+
+}
+
+id = 0
+groupName = String
+}
+
+const groupName = {
+    1: 'Group A',
+    2: 'Group B',
+    3: 'Group C'
+}
+
+module.exports = {
+    addNewGroupName
+}
