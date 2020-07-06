6d15a1ef81eb1b11c82c5f7912e939321c2dcc18
YOUR_VAMSI <vamsir@student.chalmers.se>
2020-07-06 22:16:59 +0200

    Team model

---
+++ backend/model/team.js
@@ -0,0 +1,20 @@
+
+class team{
+
+constructor(json){
+
+}
+
+id = 0
+teamName = String
+}
+
+const teamName = {
+    1: 'IFK Göteborg',
+    2: 'Manchester United',
+    3: 'Chelsea'
+}
+
+module.exports = {
+    addNewTeamName
+}
