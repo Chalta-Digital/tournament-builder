92a702d5c6cfb65e7a3e48ac19946a91d63562ff
YOUR_VAMSI <vamsir@student.chalmers.se>
2020-07-08 17:08:00 +0200

    Game model

---
+++ backend/model/game.js
@@ -0,0 +1,22 @@
+
+class game{
+
+constructor (json){
+
+}
+
+id = 0
+groupId = 0
+
+}
+
+const GAMETYPE = {
+    1: 'SPORT',
+    2: 'THROWING'
+
+}
+
+module.exports = {
+    Game,
+    addNewGame
+}
