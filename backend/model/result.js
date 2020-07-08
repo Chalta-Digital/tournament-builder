30cf1162ba241a9d7bbe353d7389ca2cdb068bac
YOUR_VAMSI <vamsir@student.chalmers.se>
2020-07-08 17:12:17 +0200

    Result model

---
+++ backend/model/result.js
@@ -0,0 +1,22 @@
+
+class result{
+
+constructor(json){
+
+
+}
+
+id = 0
+gameId = 0
+}
+
+const RESULTTYPE = {
+    1: 'WON',
+    2: 'DRAWN',
+    3: 'LOST'
+}
+
+module.exports = {
+    result,
+    addNewResultType
+}
