d9b92bbbc6e1872ad3883a3b991b33a5ff4bdc51
YOUR_VAMSI <vamsir@student.chalmers.se>
2020-07-08 13:34:48 +0200

    Team Tournament model

---
+++ backend/model/team_tournament.js
@@ -0,0 +1,22 @@
+
+class TeamTournament{
+
+constructor (json){
+
+}
+
+id = 0
+teamId = 0
+tournamentId = 0
+
+}
+
+const tournamentId = {
+    1: '7458651',
+    2: '8475878',
+
+}
+
+module.exports = {
+    addNewTournamentId
+}
