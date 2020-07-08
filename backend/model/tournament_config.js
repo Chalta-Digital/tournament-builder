7466c35f6582707ef71a8ec0a779365f7a5341a0
YOUR_VAMSI <vamsir@student.chalmers.se>
2020-07-08 17:02:26 +0200

    TournamentConfig model

---
+++ backend/model/tournament_config.js
@@ -0,0 +1,23 @@
+
+class TournamentConfig{
+
+constructor (json){
+
+}
+
+id = 0
+tournamentType = String
+numberOfGroups = 0
+numberOfStages = 0
+}
+
+const tournamentType = {
+    1: 'Group torunaments',
+    2: 'Single elimination tournament',
+    3: 'Double elimination tournament'
+
+}
+
+module.exports = {
+    addNewTournamentType
+}
