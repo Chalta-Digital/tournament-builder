c99cb60fdf1e8bfb6ba02a53ac2e477eba17ce8b
[orbachin] <sat.sust@gmail.com>
2020-07-14 21:00:35 +0100

    postgress package added, insertion db query modified, db delete script added

---
+++ backend/db_config/db_del.js
@@ -0,0 +1,35 @@
+const { Client } = require('pg');
+
+const pgclient = new Client({
+    host: process.env.POSTGRES_HOST || 'localhost',
+    port: process.env.POSTGRES_PORT || 5432,
+    user: 'api',
+    password: 'api',
+    database: 'db_tournament'
+});
+
+if(pgclient.connect()){
+	console.log('connected');
+    const deleteTablesText = `
+    DROP TYPE IF EXISTS role CASCADE;
+    DROP TABLE IF EXISTS users CASCADE;
+    DROP TABLE IF EXISTS tournaments CASCADE;
+    DROP TABLE IF EXISTS tournament_config CASCADE;
+    DROP TABLE IF EXISTS team_tournament CASCADE;
+    DROP TABLE IF EXISTS teams CASCADE;
+    DROP TABLE IF EXISTS games CASCADE;
+    DROP TABLE IF EXISTS groups CASCADE;
+    DROP TABLE IF EXISTS results CASCADE;
+    `
+    
+	pgclient.query(deleteTablesText, (err, res) => {
+        if (err) throw err;
+    });
+
+
+}else(
+    console.log('Problem with DB connection')
+);
+
+
+
--- backend/db_config/db_init.js
+++ backend/db_config/db_init.js
@@ -1,34 +1,33 @@
 const { Client } = require('pg');
 
 const pgclient = new Client({
-    host: process.env.POSTGRES_HOST || ,
+    host: process.env.POSTGRES_HOST || 'localhost',
     port: process.env.POSTGRES_PORT || 5432,
-    user: 'postgres',
-    password: 'postgres',
-    database: 'postgres'
+    user: 'api',
+    password: 'api',
+    database: 'db_tournament'
 });
 
 if(pgclient.connect()){
 	
-	const createRoleEnumText = `CREATE TYPE role AS ENUM ('appAdmin', 'tournamentAdmin', 'referee');`
-	pgclient.query(createEnumText, (err, res) => {
-		if (err) throw err
-	});
+    const createRoleEnumText = 'CREATE TYPE role AS ENUM (\'appAdmin\', \'tournamentAdmin\', \'referee\');';
+    pgclient.query(createRoleEnumText, (err, res) => {
+        if (err) throw err;
+    });
 
-	
 	const createUserTableText  = 
 		`CREATE TABLE IF NOT EXISTS users (
 			id SERIAL PRIMARY KEY,
 			username VARCHAR (50) UNIQUE NOT NULL,
 			password  TEXT  NOT NULL,
-			user_role  role
+			user_role role
 		);
-		`
-	pgclient.query(createUserTableText, (err, res) => {
-		if (err) throw err
-	});
+		`;
+    pgclient.query(createUserTableText, (err, res) => {
+        if (err) throw err;
+    });
 	
-	const createTournamentTableText = 
+    const createTournamentTableText = 
 		`CREATE TABLE IF NOT EXISTS tournaments (
 			id SERIAL PRIMARY KEY,
 			user_id INTEGER REFERENCES users(id),
@@ -38,12 +37,12 @@ if(pgclient.connect()){
 			start_date DATE NOT NULL DEFAULT CURRENT_DATE,
 			end_date DATE CHECK (end_date > start_date)	
 		);
-		`
-	pgclient.query(createTournamentTableText, (err, res) => {
-		if (err) throw err
-	});
+		`;
+    pgclient.query(createTournamentTableText, (err, res) => {
+        if (err) throw err;
+    });
 		
-	const createTournamentConfigTableText = 
+    const createTournamentConfigTableText = 
 	`CREATE TABLE IF NOT EXISTS tournament_config (
 		id SERIAL PRIMARY KEY,
 		tournament_id INTEGER REFERENCES tournaments(id),
@@ -53,45 +52,45 @@ if(pgclient.connect()){
 		start_date DATE NOT NULL DEFAULT CURRENT_DATE,
 		end_date DATE CHECK (end_date > start_date)	
 	);
-	`
-	pgclient.query(createTournamentTableText, (err, res) => {
-		if (err) throw err
-	});
+	`;
+    pgclient.query(createTournamentTableText, (err, res) => {
+        if (err) throw err;
+    });
 
-	const createTeamsText = 
+    const createTeamsText = 
 	`CREATE TABLE IF NOT EXISTS teams (
 		id SERIAL PRIMARY KEY,
 		team_name TEXT NOT NULL
 	);
-	`
-	pgclient.query(createTeamsText, (err, res) => {
-		if (err) throw err
-	});
+	`;
+    pgclient.query(createTeamsText, (err, res) => {
+        if (err) throw err;
+    });
 
-	const createGroupsText = 
+    const createGroupsText = 
 	`CREATE TABLE IF NOT EXISTS groups (
 		id SERIAL PRIMARY KEY,
 		group_name TEXT NOT NULL,
 		tournament_id INTEGER REFERENCES tournaments(id),
 		teams INTEGER[]
 	);
-	`
-	pgclient.query(createGroupsText, (err, res) => {
-		if (err) throw err
-	});
+	`;
+    pgclient.query(createGroupsText, (err, res) => {
+        if (err) throw err;
+    });
 
-	const createTeamTournamentText = 
+    const createTeamTournamentText = 
 	`CREATE TABLE IF NOT EXISTS team_tournament (
 		id SERIAL PRIMARY KEY,
 		team_id INTEGER REFERENCES teams(id),
 		tournament_id INTEGER REFERENCES tournaments(id)
 	);
-	`
-	pgclient.query(createTeamTournamentText, (err, res) => {
-		if (err) throw err
-	});
+	`;
+    pgclient.query(createTeamTournamentText, (err, res) => {
+        if (err) throw err;
+    });
 
-	const createGamesText = 
+    const createGamesText = 
 	`CREATE TABLE IF NOT EXISTS games (
 		id SERIAL PRIMARY KEY,
 		group_id INTEGER REFERENCES groups(id),
@@ -99,30 +98,30 @@ if(pgclient.connect()){
 		team_id_1 INTEGER NOT NULL,
 		team_id_2 INTEGER NOT NULL
 	);
-	`
-	pgclient.query(createGamesText, (err, res) => {
-		if (err) throw err
-	});
+	`;
+    pgclient.query(createGamesText, (err, res) => {
+        if (err) throw err;
+    });
 
-	const createResultsText = 
+    const createResultsText = 
 	`CREATE TABLE IF NOT EXISTS results (
 		id SERIAL PRIMARY KEY,
 		game_id INTEGER REFERENCES games(id),
 		team_id_1_score INTEGER NOT NULL,
 		team_id_2_score INTEGER NOT NULL
 	);
-	`
-	pgclient.query(createResultsText, (err, res) => {
-		if (err) throw err
-	});
+	`;
+    pgclient.query(createResultsText, (err, res) => {
+        if (err) throw err;
+    });
 
 
-	/** 
+    /** 
 	 dummy data generation for users table 
 	*/
-	const generateUsersTableDataText = 
+    const generateUsersTableDataText = 
 	`
-	INSERT INTO users(username, password)
+	INSERT INTO users(username, password, role)
 	SELECT
 	  'user_' || seq AS username,
 	  'chalta2020' AS password,
@@ -134,15 +133,15 @@ if(pgclient.connect()){
 		END
 	  ) AS role
 	FROM GENERATE_SERIES(1, 5) seq;
-	`
-	pgclient.query(generateUsersTableDataText, (err, res) => {
-		if (err) throw err
-	});
+	`;
+    pgclient.query(generateUsersTableDataText, (err, res) => {
+        if (err) throw err;
+    });
 
 
 }else(
-	console.log('Problem with DB connection')
-)
+    console.log('Problem with DB connection')
+);
 
 
 
