1978e9fc2a0ba4c929a01feb742e4347d3793cf7
[orbachin] <sat.sust@gmail.com>
2020-07-17 11:17:27 +0100

    fine tuning dashboard

--- frontend/src/views/Dashboard.vue
+++ frontend/src/views/Dashboard.vue
@@ -1,7 +1,36 @@
 <template>
-  <div>
-  TM Dashboard
-  </div>
+   <v-card
+    class="mt-4 mx-auto"
+    max-width="800"
+  >
+    <v-sheet
+      class="v-sheet--offset mx-auto"
+      color="cyan"
+      elevation="12"
+      max-width="calc(100% - 32px)"
+    >
+      <v-sparkline
+        :labels="labels"
+        :value="value"
+        color="white"
+        line-width="2"
+        padding="16"
+      ></v-sparkline>
+    </v-sheet>
+
+    <v-card-text class="pt-0">
+      <div class="title font-weight-light mb-2">User Dashboard</div>
+      <div class="subheading font-weight-light grey--text">Last Campaign Performance</div>
+      <v-divider class="my-2"></v-divider>
+      <v-icon
+        class="mr-2"
+        small
+      >
+        mdi-clock
+      </v-icon>
+      <span class="caption grey--text font-weight-light">last registration 26 minutes ago</span>
+    </v-card-text>
+  </v-card>
 </template>
 
 <script>
